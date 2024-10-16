5b9f3a71a84484b196292c4a2c21131b5a351b5a
kkishumi <linestrell@gmail.com>
2024-10-17 03:07:38 +0500

    add app

---
+++ src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+// Класс Main для запуска программы
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database();
+        Menu menu = new Menu(database);
+        menu.displayMenu();
+    }
+}
